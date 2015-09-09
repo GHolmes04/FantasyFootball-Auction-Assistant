285d932d18c913ec42a89ce556d12b4da31cdf7b
yourUsername <gregHolmes04@gmail.com>
2015-09-09 06:58:07 -0400

    Adds Angular Framework and BoilerPlate

---
+++ app/app.js
@@ -0,0 +1,22 @@
+// (function FantasyFootballAuctionAppIIFE(){
+//   var app = angular.module('fantasyFootballAuctionApp', ['ngRoute']);
+
+//   app.config(function($routeProvider){
+//     $routeProvider
+//       .when('/',
+//             {
+//               controller: 'homeController as homeCtrl',
+//               templateUrl: 'app/views/home.html'
+//             }
+//            )
+//       // .when('/,
+//       //       {
+//       //         controller: 'ordersController',
+//       //         controllerAs: 'ordersCtrl',
+//       //         templateUrl: 'app/views/orders_done.html'
+//       //       }
+//       //      )
+//       .otherwise({redirectTo: '/'});
+//   });
+
+// })();
---
+++ app/services/values.js
@@ -0,0 +1,4 @@
+angular.module("FantasyFootballAuctionApp").value('appSettings', {
+  title: "Fantasy Football Auction App",
+  version: "1.0"
+});
